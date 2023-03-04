90c00c720f56f4944a7213b13941c9773800d8cc
Player10202 <114907799+Player10202@users.noreply.github.com>
2023-03-04 20:18:21 +1100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,40 +1,19 @@
+var canvas, backgroundImage;
 
-function setup() {
-  createCanvas(400,400);
-  background(51);
-  box = createSprite(200,200,30,30);
+var questions;
 
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.start();
 }
 
-function draw() 
-{
 
-  // write the code to change background color 
-  // to red when RIGHT_ARROW is pressed
-  
-  
+function draw(){
+  background("pink");
 
-  if (keyIsDown(LEFT_ARROW)) 
-  {
-    background("blue");
-    
-  }
- 
-    if (keyIsDown(UP_ARROW)) 
-  {
-    background("yellow");
-   
-  }
-
-  if (keyIsDown(DOWN_ARROW)) 
-  {
-    background("green");
-  }
-if(keyIsDown(RIGHT_ARROW))
-{
-background("red")
 }
-  
-  drawSprites();
-}
-
